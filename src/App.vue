15b798c217ebe94b93626b21322d60a488810f94
Daniil <borikin.daniil@gmail.com>
2023-09-10 09:34:16 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div id="detection"
+         style="display: none; background-color: canvas; color-scheme: light">
+    </div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -12,7 +15,23 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+
+  mounted() {
+    if (window.matchMedia) {
+      console.log('window.matchMedia', window.matchMedia)
+      const isDarkFromOS = window.matchMedia('prefers-color-scheme: dark').matches;
+      console.log('isDarkFromOS', isDarkFromOS)
+
+      const detectionDiv = document.querySelector('#detection');
+      // If the computed style is not white then the page is in Auto Dark Theme.
+      const isForcedDarkChrome = getComputedStyle(detectionDiv).backgroundColor != 'rgb(255, 255, 255)';
+      console.log('isForcedDarkChrome', isForcedDarkChrome)
+
+      const isChrome = navigator.userAgent.indexOf("Chrome") != -1;
+      console.log('isChrome', isChrome)
+    }
+  },
 }
 </script>
 
